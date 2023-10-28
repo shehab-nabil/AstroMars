f8ef76866d53d298aec45ef440442b4f91652f92
shehab nabil <shehabnabil790@gmail.com>
2023-10-28 18:07:32 +0200

    cancel the device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 import 'astromars.dart';
 
 void main() {
-  runApp(DevicePreview(
-    builder: (BuildContext context) => const AstroMars(),
-  ));
+  runApp(
+    const AstroMars(),
+  );
 }
